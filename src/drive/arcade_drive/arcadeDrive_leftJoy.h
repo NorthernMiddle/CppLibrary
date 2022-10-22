046dcea9b4199c9122e6563bd03f2ef47443d8ff
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-22 14:39:24 -0400

    Create arcadeDrive_leftJoy.h

---
+++ src/drive/arcade_drive/arcadeDrive_leftJoy.h
@@ -0,0 +1,9 @@
+#ifndef ARCADEDRIVELEFTJOY_H
+#define ARCADEDRIVELEFTJOY_H
+
+int cube_drive_l_leftjoy(int drive_l_leftjoy);				  // arcade drive cube scale factor left joystick control
+int cube_drive_r_leftjoyl(int drive_r_leftjoy);				  // arcade drive cube scale factor left joystick control
+void deadBand_arcadeLeftJoy();
+void arcadeDrive_leftJoy();
+
+#endif
