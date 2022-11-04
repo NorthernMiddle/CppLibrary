7401854f0220aae5dd5263a01f6a3f8185164a5b
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-11-04 08:56:03 -0400

    Delete arcadeDrive_rightJoy.h

--- src/drive/arcade_drive/arcadeDrive_rightJoy.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARCADEDRIVERIGHTJOY_H
-#define ARCADEDRIVERIGHTJOY_H
-
-int cube_drive_l_rightjoy(int drive_l_rightjoy);			 // arcade drive cube scale factor right joystick control
-int cube_drive_r_rightjoyl(int drive_r_rightjoy);			// arcade drive cube scale factor right joystick control
-void deadBand_arcadeRightJoy();
-void arcadeDrive_rightJoy();
-
-#endif
