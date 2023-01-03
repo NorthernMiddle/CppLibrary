664799f771405f2b89ad541d0b276b5bbf7b844c
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2023-01-03 12:40:33 -0500

    Update pidMartinMa.cpp

--- src/pid/pidMartinMa.cpp
+++ src/pid/pidMartinMa.cpp
@@ -3,13 +3,14 @@
 #include "vex.h"
 using namespace vex;
 
-// define Motor Gearing
-#define VEX393Torque    627.2
-#define VEX393Standard  392.0
-#define VEX393Turbo     261.333
-
 // define PI
 #define PI = 3.14159265359;
+
+// define V5 Motor Gearing
+#define BLUE_MOTOR_CARTRIDGE 2.4   // turbo
+#define GREEN_MOTOR_CARTRIDGE 1.6  // highspeed
+#define RED_MOTOR_CARTRIDGE 1      // torque
+
 
 /**
   * 
@@ -22,11 +23,33 @@ void moveBase(double speed)
 }
 
 
+
+/**
+ * Converts inches to quadrature encoder ticks, for the purpose of setting targets in PIDs
+ * Usage:
+ *  inchesToTicks(10, 3.25, 1, TURBO);
+ *
+ * @param float unit The number inches to convert to ticks
+ * @param float wheelDiameter The diameter of the wheel, in inches
+ * @param float gearing The external gear ration (Speed Up => x, Torque Up => 1/x)
+ * @param float motorGear The internal gear ratio of the motor (constants TURBO, HIGHSPEED, and TORQUE have been specified)
+ */
+float inchesToTicks(float unit, float wheelDiameter, float gearing, float motorGear) 
+{
+    return (
+        (unit / (motorGear * gearing)) / // Cancel out gear ratio
+        (wheelDiameter * PI) // Divide by circumfrence to solve for rotations
+    ) * 360; // 360 ticks in a rotation
+}
+
 /**
   * 
   * reading: ticks -- 393 motors: VEX393Torque -- 627.2 ticks/rev
-  * 1 turn of wheels -- 4 inch wheels -- 627.2 * 2 ticks
+  * 1 turn of wheels -- 4 inch wheels -- 627.2 * 2ticks
   * 1 turn of each wheel 4*PI inches
+  * 4*PI inches = 627.2 * 2ticks
+  *
+  * ticks/inch = (627.2 * 2) / (4 * PI) = 99.82198
   * 
   */
 void pidBaseControl(double target)
